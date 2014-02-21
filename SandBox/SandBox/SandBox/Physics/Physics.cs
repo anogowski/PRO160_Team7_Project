7e023722f9434ab48adc458320d3a9fd4cbf3998
SuperAwesomeMane <ashtinhle@hotmail.com>
2014-02-21 11:08:50 -0700

    Moved some files around so that everything works

--- SandBox/SandBox/SandBox/Physics/Physics.cs
+++ SandBox/SandBox/SandBox/Physics/Physics.cs
@@ -5,13 +5,12 @@ using System.Text;
 using Microsoft.Xna.Framework;
 using PhysicsSandbox.Shapes;
 
-namespace PhysicsSandbox.Physics {
-	public class Physics {
-		//general forces
-		public float Gravity {
-			get;
-			set;
-		}
+namespace PhysicsSandbox.Physics
+{
+    public class Physics
+    {
+        //general forces
+        public float Gravity { get; set; }
 
 		/// <summary>
 		/// Air resistance is a value between 0 and 1 that slows down the velocity of an object.
@@ -21,22 +20,24 @@ namespace PhysicsSandbox.Physics {
 			set;
 		}
 
-		public Physics(float gravity = 9.8f, float airResistance = 0.99f) {
-			Gravity = gravity;
-			AirResistance = airResistance;
-		}
+        public Physics(float gravity = 9.8f)
+        {
+ 
+            Gravity = gravity;
+        }
 
-		public void addGeneralForces<T>(ref T shape) where T : Shape {
-			addFreeFall(ref shape);
-			addAirResistance(ref shape);
-		}
+        public void addGeneralForces<T>(ref T shape) where T : Shape
+        {
+            addFreeFall(ref shape);
+        }
 
-		private void addFreeFall<T>(ref T shape) where T : Shape {
-			shape.Velocity = new Vector3(shape.Velocity.X, (Gravity * shape.MoveTime * shape.MoveTime) / 2, 0);
-		}
+        private void addFreeFall<T>(ref T shape) where T : Shape
+        {
+            shape.Velocity = new Vector3(shape.Velocity.X, (Gravity * shape.MoveTime * shape.MoveTime) / 2, 0);            
+        }
 
 		public void addAirResistance<T>(ref T shape) where T : Shape {
 			shape.Velocity *= AirResistance;
 		}
-	}
+    }
 }
