e981d0790c3a766ee687503d58486613517a3255
Adam Nogowski <anogowski_neumont@live.com>
2014-02-27 10:12:45 -0700

    Moved Patient outside of users

--- Medical_System/Medical/Diseases.cs
+++ Medical_System/Medical/Diseases.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Medical_System.Medical
 {
-    class Diseases
+    public static class Diseases
     {
+        ObservableCollection<string> Name = new ObservableCollection<string>();
         
     }
 }
