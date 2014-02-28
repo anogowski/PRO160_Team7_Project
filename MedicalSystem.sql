4c5eeef9092c110d2a4e6353c65c468eb90785d8
wozhendeaa <billyyang520@gmail.com>
2014-02-28 10:50:43 -0700

    new database sql file

--- MedicalSystem.sql
+++ MedicalSystem.sql
@@ -6,7 +6,7 @@ use MedicalSystem
 create table BloodType
 (
 	BID int identity(1,1) primary key,
-	TypeName nchar(10) default('A')
+	TypeName nchar(10) default('A') not null, 
 )
 
 create table Medicine
@@ -21,7 +21,7 @@ create table Perscription
 	PERS_ID int identity(1,1) primary key, 
 	DID int,
 	PID int ,
-	DateIssued datetime default(getdate()),
+	DateIssued datetime default(getdate()) not null,
 	Note text null,
 	Reactions text null
 )
@@ -69,13 +69,20 @@ create table Doctor
 	foreign key (FID) references Fields(FID),
 	HomeAddress nchar(100), 
 	PhoneNumber nchar(15), 
-	
+)
+
+create table DiseaseType
+(
+	DTID int identity(1,1) primary key, 
+	Name char(50)
 )
 
 create table Disease
 (
 	DEAID int identity(1,1) primary key, 
 	Name char(50) not null,
+	Disease_type int constraint fk_disease_type 
+		foreign key (Disease_type) references DiseaseType(DTID)
 )
 
 create table Disease_Patient
