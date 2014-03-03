49ef6bdc0a962ae4964ac1da109ea37282f36a12
wozhendeaa <billyyang520@gmail.com>
2014-03-03 09:34:13 -0700

    Data helper

--- Medical_System/DbHelper.cs
+++ Medical_System/DbHelper.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Medical_System.Users;
+using Medical_System.Models;
 
 namespace Medical_System
 {
     public class DbHelper
     {
-        public List<Doctor>
+        public List<Doctor> GetDoctors()
+        {
+            using(var context = new MedicalSystemContext())
+            {
+                var list = from d in context.Doctors
+                           select d;
+                return list.ToList<Doctor>();
+            }
+
+        }
     }
 }
