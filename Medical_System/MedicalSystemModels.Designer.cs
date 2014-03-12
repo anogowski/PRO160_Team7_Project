b8df79b5cde438f4189856beb8e10e609f57011b
Adam Nogowski <anogowski_neumont@live.com>
2014-03-12 11:33:05 -0600

    Remake your models and dont commit them ever

--- Medical_System/MedicalSystemModels.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Default code generation is disabled for model 'S:\Neumont\PRO160\Team7\Medical_System\MedicalSystemModels.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
